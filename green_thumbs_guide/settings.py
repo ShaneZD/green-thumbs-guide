f9ac4711f14ba6a6286e34908994ba655b6db381
ShaneZD <146131214+ShaneZD@users.noreply.github.com>
2024-10-15 01:11:59 +0100

    Update settings.py

--- green_thumbs_guide/settings.py
+++ green_thumbs_guide/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
