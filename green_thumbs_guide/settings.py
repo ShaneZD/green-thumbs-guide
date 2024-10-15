4d2607d56d29d3be51f5d973f89dd96b703d5c7d
ShaneZD <146131214+ShaneZD@users.noreply.github.com>
2024-10-15 01:05:39 +0100

    Update settings.py

--- green_thumbs_guide/settings.py
+++ green_thumbs_guide/settings.py
@@ -110,11 +110,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files settings (CSS, JavaScript, Images)
-STATIC_URL = '/static/'  
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
