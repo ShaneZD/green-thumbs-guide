6f485366bb6e678cde619d59591d7bfac4e46224
ShaneZD <146131214+ShaneZD@users.noreply.github.com>
2024-10-15 01:13:14 +0100

    Update wsgi.py
    added whitenoise to wsgi.py

--- green_thumbs_guide/wsgi.py
+++ green_thumbs_guide/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for green_thumbs_guide project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'green_thumbs_guide.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application)
